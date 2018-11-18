87247dc3a0097438ffbb4d3baa4151e8cb0e09e1
Yu Zhang <clarazhang@gmail.com>
2018-11-18 08:47:14 +0800

    [lab2] Added the version used in 2017 fall

---
+++ c1interpreter/src/assembly_builder.cpp
@@ -0,0 +1,63 @@
+
+#include "assembly_builder.h"
+
+#include <vector>
+
+using namespace llvm;
+using namespace c1_recognizer::syntax_tree;
+
+void assembly_builder::visit(assembly &node)
+{
+}
+
+void assembly_builder::visit(func_def_syntax &node)
+{
+}
+
+void assembly_builder::visit(cond_syntax &node)
+{
+}
+
+void assembly_builder::visit(binop_expr_syntax &node)
+{
+}
+
+void assembly_builder::visit(unaryop_expr_syntax &node)
+{
+}
+
+void assembly_builder::visit(lval_syntax &node)
+{
+}
+
+void assembly_builder::visit(literal_syntax &node)
+{
+}
+
+void assembly_builder::visit(var_def_stmt_syntax &node)
+{
+}
+
+void assembly_builder::visit(assign_stmt_syntax &node)
+{
+}
+
+void assembly_builder::visit(func_call_stmt_syntax &node)
+{
+}
+
+void assembly_builder::visit(block_syntax &node)
+{
+}
+
+void assembly_builder::visit(if_stmt_syntax &node)
+{
+}
+
+void assembly_builder::visit(while_stmt_syntax &node)
+{
+}
+
+void assembly_builder::visit(empty_stmt_syntax &node)
+{
+}
